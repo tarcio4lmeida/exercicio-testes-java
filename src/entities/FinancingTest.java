93114feacd0f2ad7b0d77b25d4028887fa7bd22e
tarcio almeida <tarcioalmeidalima@gmail.com>
2024-01-14 22:13:39 -0300

    Construindo os testes do método setIncome

--- src/entities/FinancingTest.java
+++ src/entities/FinancingTest.java
@@ -22,18 +22,38 @@ class FinancingTest {
 
     //Deve atualizar o valor quando os dados forem válidos
     @Test
-    public void deveAtualizarOValorQuandoOsDadosForemValidos(){
+    public void setTotalAmountDeveAtualizarOValorQuandoOsDadosForemValidos(){
         Financing f1 = new Financing(100000.0, 2210.0, 80);
         f1.setTotalAmount(110000.0);
 
     }
     //Deve lançar IllegalArgumentException quando os dados não forem válidos
     @Test
-    public void deveLancarIllegalArgumentExceptionQuandoOValorQuandoOsDadosForemInValidos(){
+    public void setTotalAmountDeveLancarIllegalArgumentExceptionQuandoOValorQuandoOsDadosForemInValidos(){
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             Financing f1 = new Financing(100000.0, 2210.0, 80);
             f1.setTotalAmount(120000.0);
         });
     }
+
+    // Deve atualizar o valor quando os dados forem válidos
+    @Test
+    public void setIncomeDeveAtualizarOValorQuandoOsDadosForemValidos(){
+        Financing f1 = new Financing(100000.0, 2210.0, 80);
+        f1.setIncome(2220.0);
+
+    }
+    //Deve lançar IllegalArgumentException quando os dados não forem válidos
+    @Test
+    public void setIncomeAmountDeveLancarIllegalArgumentExceptionQuandoOValorQuandoOsDadosForemInValidos(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing f1 = new Financing(100000.0, 2210.0, 80);
+            f1.setIncome(1110.0);
+        });
+    }
+
+
+
+
 
 }
